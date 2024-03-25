aa2505837e9ea2f75a4429cd15e8705aa66449bb
LamaAQawareeq <lamaqawareeq123@gmail.com>
2024-03-25 16:56:43 +0300

    as

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ const app = express();
 const mydb = require('./config/db');
 const rout = require('./routes/router');
 const path=require("path")
-const projectRoutes = require('./routes/routerProject');
+const  projectRoutes = require('./routes/routerProject');
 
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(rout);
 
-app.use("/allproject/skills/projectID/picture/ofCompletaion" ,express.static(path.join(__dirname,"images"))); //localhost:3001allproject/skills/projectID/picture/ofCompletaion/tat.jpg
+app.use("/allproject/skills/projectID/picture/ofCompletaion" ,express.static(path.join(__dirname,"images")));  //localhost:3001allproject/skills/projectID/picture/ofCompletaion/tat.jpg
 app.use("/allproject/skills/projectID/picture/ofMaterial" ,express.static(path.join(__dirname,"images"))); //localhost:3001allproject/skills/projectID/picture/ofCompletaion/tat.jpg
 app.use("/allproject/skills/addpicture" ,require("./routes/upload")); // to upload in postman
 
