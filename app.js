38d5d56d04ff4da6be77ecb33c149599e8fe2c44
LamaAQawareeq <lamaqawareeq123@gmail.com>
2024-03-25 17:01:47 +0300

    hh

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(rout);
 
-app.use("/allproject/skills/projectID/picture/ofCompletaion" ,express.static(path.join(__dirname,"images")));  //localhost:3001allproject/skills/projectID/picture/ofCompletaion/tat.jpg
+app.use ("/allproject/skills/projectID/picture/ofCompletaion" ,express.static(path.join(__dirname,"images")));  //localhost:3001allproject/skills/projectID/picture/ofCompletaion/tat.jpg
 app.use("/allproject/skills/projectID/picture/ofMaterial" ,express.static(path.join(__dirname,"images"))); //localhost:3001allproject/skills/projectID/picture/ofCompletaion/tat.jpg
 app.use("/allproject/skills/addpicture" ,require("./routes/upload")); // to upload in postman
 
